43851962fe1c9f074f9b02ed698d3eaebdba9f25
Senfire <eric.mcghee@northwestern.edu>
2023-10-20 23:38:23 -0500

    Checking local storage for isFirstTime key entry

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,9 +20,21 @@ var imdbKey="57df2f59f73d6513b02f8a10cd393e77"
 // welcome Modal close
 var span = document.getElementById("welcomeClose");
 
-//Close modal when user clicks on the span
+// Close modal when user clicks on the span
 span.onclick = function() {
     modal.style.display = "none";
     localStorage.setItem("isFirstTime", false);
 }
 
+// When user clicks anywhere outside the modal, close the modal window.
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        localStorage.setItem("isFirstTime", false);
+    }
+}
+
+// Check isFirstTime key for modal request - display welcome modal if isFirstTime is true
+function keyCheck() {
+    if (localStorage.getItem("isFirstTime")=== null){modal.style.display="block"}
+}
